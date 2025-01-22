cdd69e36a18c4d6acfd18a297ba9ad886cbd1e0d
levy <l.muguro@chem-labs.com>
2025-01-22 12:29:01 +0300

    feat(certificate): designed template

--- generatepdf.py
+++ generatepdf.py
@@ -1,6 +1,19 @@
+# from weasyprint import HTML
+# import sys
+
+# html = HTML(filename='./certs.html')
+
+# HTML(file_obj=sys.stdin)
+
+# html.write_pdf('/certs.pdf')
+
 from weasyprint import HTML, CSS
 from weasyprint.text.fonts import FontConfiguration
 
+font_config = FontConfiguration()
 html = HTML(filename='./certs.html')
 css = CSS(filename='./certs.css')
-html.write_pdf('./cert.pdf', stylesheets=[css])
+html.write_pdf(
+    './cert.pdf', stylesheets=[css],
+    font_config=font_config)
+
